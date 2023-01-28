3a5edc125f4dd9d76a69552a730aef2542d2afd0
ZacariasQG <115042600+ZacariasQG@users.noreply.github.com>
2023-01-28 04:18:12 -0300

    Jugando con objetos

---
+++ JS/jugando con objetos/jugandoConObjetos.js
@@ -0,0 +1,24 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}, {name: "Severa", age: 15}];
+
+// ¿Cómo harías print/log de la edad de John?
+
+console.log(users[1].age);
+
+// ¿Cómo harías print/log del nombre del primer objeto?
+
+console.log(users[0].name);
+
+
+// ¿Cómo harías print/log del nombre y la edad de cada usuario utilizando un for loop? Tu output debería verse algo como esto
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].name + " - " + users[i].age)
+}
+
+// ¿Cómo harías para imprimir el nombre de los mayores de edad?
+
+for (let i = 0; i < users.length; i++) {
+    if (users[i].age >= 18) {
+        console.log(users[i].name + " - " + users[i].age)
+    }
+}
